4fb9c4f24fb1e3c43396d5431ea0443d506d54dd
akakss225 <akakss225@naver.com>
2021-03-15 16:44:01 +0900

    파이썬 기초 조건문2

---
+++ 제어문/조건문2.py
@@ -0,0 +1,19 @@
+if 1 :
+    print("참")
+else :
+    print("거짓")
+
+a = 1 
+b = 2
+
+if b < a :
+    print("참")
+else :
+    print("거짓")
+
+money = 2000
+
+if money >= 3000 :
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
