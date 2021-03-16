6923faad4a420b728f91e801e24f90d6adeb8e5b
akakss225 <akakss225@naver.com>
2021-03-16 20:09:16 +0900

    파이썬 기초 Class2

---
+++ 객체지향/Class2.py
@@ -0,0 +1,18 @@
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def minus(self):
+        result = self.first - self.second
+        return result
+
+
+a = FourCal()
+a.setdata(1,2)
+print(a.first)
+print(a.second)
+print(a.add())
+print(a.minus())
