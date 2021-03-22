ad925926390bc713601ea1d9f14529cbb45f63f1
akakss225 <akakss225@naver.com>
2021-03-22 19:42:27 +0900

    파이썬 반복학습13

---
+++ 나동빈수업/base16_람다식.py
@@ -0,0 +1,14 @@
+# 쉬운 함수의 경우 람다식을 이용해 매우 간결하게 만들 수 있음.
+
+add = lambda x, y: x + y
+
+print(add(1,2))
+
+# map() : 다수의 원소에 대한 함수의 결과를 한번에 얻을 수 있도록 도와줌
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9,10]
+
+my_function = lambda a, b : a +b
+result = map(my_function, list1, list2)
+print(result)
+print(list(result))
