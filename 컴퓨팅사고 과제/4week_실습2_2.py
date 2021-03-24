c287f4dd5e95893f2ab22af60da45cd38f44309a
akakss225 <akakss225@naver.com>
2021-03-24 18:54:18 +0900

    컴퓨팅사고_4weeks_3re

--- 컴퓨팅사고 과제/4week_실습2_2.py
+++ 컴퓨팅사고 과제/4week_실습2_2.py
@@ -5,12 +5,9 @@ for i in range(0,5):
     list[i] = int(f.readline())
     print(list)
 
-data = 0
-for i in range(0,5):
-    data += list[i]
 
-print(data)
-print(data/5)
+print(sum(list))
+print(sum(list)/5)
 
 f.close()
 
