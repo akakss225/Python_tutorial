82a2537615e18c05cb4effbea5675e520205091b
akakss225 <akakss225@naver.com>
2021-03-24 18:49:16 +0900

    Python rebase_Dictionary

---
+++ python기초/rebase1.py
@@ -0,0 +1,13 @@
+dic = {'Kor': 80,'Eng' : 90,'Math' : 77}
+
+print(dic.items())
+
+dic['Bio'] = 100
+
+print(dic.items())
+
+if 'Bio' in dic:
+    print('생물학 시험을 치뤘습니다.')
+else:
+    print('생물학 시험을 치루지 않았습니다.')
+
