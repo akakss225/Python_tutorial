3d2f2fe516823742faa3002c1cfc4cd29b999a97
akakss225 <akakss225@naver.com>
2021-03-31 16:53:41 +0900

    Pythom Algorithm_selection sort

---
+++ 컴퓨팅사고 과제/03_31_5week.py
@@ -0,0 +1,20 @@
+# 자료구조
+# Queue : 선입선출의 구조.
+# Stack : 후입선출의 구조.
+
+
+def selectionSort(value):
+    length = len(value)
+    for i in range(length - 1):
+        indexMin = i
+        for j in range(i, length):
+            if value[indexMin] > value[j]:
+                indexMin = j
+        value[i], value[indexMin] = value[indexMin], value[i]
+        print('step',i,'-',value)
+
+a = [3,6,4,1,2,5,9,8,7]
+
+print(a)
+
+print(selectionSort(a))
