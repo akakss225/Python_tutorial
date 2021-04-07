c600a1a054691e7304279eda40fedb10bd7baf6c
akakss225 <akakss225@naver.com>
2021-04-07 19:03:59 +0900

    PyQT 스타트1

---
+++ PyQT/num1.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import *
+import PyQt5 import uic
+
+form_class = uic.loadUiType(C:\\Users\\akaks\\Documents\\Python_tutorial\\PyQT\\num1.ui)[0]
+
+class WindowClass(QMainWidow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+app = QApplcation(sys.argv)
+mainWindow = WindowClass()
+mainWindow.show()
+app.exec_()
