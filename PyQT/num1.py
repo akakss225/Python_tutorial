51e4a9199aa521fe9ef3bb5a1e1967f905974f73
akakss225 <akakss225@naver.com>
2021-04-09 14:44:21 +0900

    PyQT_num1_완

--- PyQT/num1.py
+++ PyQT/num1.py
@@ -4,12 +4,12 @@ from PyQt5 import uic
 
 form_class = uic.loadUiType("C:\\Users\\akaks\\Documents\\Python_tutorial\\PyQT\\num1.ui")[0]
 
-class WindowClass(QMainWidow, from_class):
+class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         
-app = QApplcation(sys.argv)
+app = QApplication(sys.argv)
 mainWindow = WindowClass()
 mainWindow.show()
 app.exec_()
---
+++ 자료구조 과제/picture.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow,QApplication, qApp, QFileDialog
+from PyQt5.uic import loadUiType
+from PyQt5.QtGui import *
+
+form_class = loadUiType("imageViewer.ui")[0]
+
+class ViewerClass(QMainWindow, form_class):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self,parent)
+        self.setupUi(self)
+        self.actionSelect.triggered.connect(self.fileSelect)
+        self.actionExit.triggered.connect(qApp.quit)
+
+    def fileSelect(self):
+        fname = QFileDialog.getOpenFileName(self)
+        qPixmapVar = QPixmap()
+        qPixmapVar.load(fname[0])
+        qPixmapVar = qPixmapVar.scaled(700, 400, aspectRatioMode=True)
+        self.label.setPixmap(qPixmapVar)
+
+app = QApplication(sys.argv)
+myWindow = ViewerClass(None)
+myWindow.show()
+app.exec_()
