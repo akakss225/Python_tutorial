f04eec8af21088a6d9f1d26bf8d714f813b17204
akakss225 <akakss225@naver.com>
2021-04-14 18:52:49 +0900

    Queue_Base Structure

--- DataStructure/num1.py
+++ DataStructure/num1.py
@@ -3,11 +3,49 @@
 # enQueue : Queue를 늘리는것
 # deQueue : Queue를 줄이는것
 
-queue = []
+class Queue:
+    def __init__(self):
+        self.q = []
+    
+    def enQueue(self, item):
+        self.q.append(item)
+    
+    def deQueue(self):
+        if self.isEmpty() == True:
+            return None
+        else:
+            return self.q.pop(0)
+    
+    def size(self):
+        return len(self.q)
+    
+    def isEmpty(self):
+        return not self.q
+    
+    #가장 먼저 추가된 데이터
+    def getFront(self):
+        if self.isEmpty() == True:
+            return None
+        else:
+            return self.q[0]
+    
+    #가장 나중에 추가된 데이터
+    def getRear(self):
+        if self.isEmpty() == True:
+            return None
+        else:
+            return self.q[-1]
+    
 
-queue.append(10)
-queue.append(20)
-queue.append(30)
-print(queue)
-print(queue.pop(0))
-print(queue)
+q = Queue()
+print(q.isEmpty())
+print(q.deQueue())
+q.enQueue(10)
+q.enQueue(20)
+q.enQueue(30)
+print(q.q)
+print(q.getFront())
+print(q.getRear())
+
+print(q.deQueue())
+print(q.q)
