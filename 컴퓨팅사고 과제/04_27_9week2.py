eb6c7b442be612372770db8427076ff3f542da33
akakss225 <akakss225@naver.com>
2021-04-27 20:46:41 +0900

    컴퓨팅사고 04_27_class2

---
+++ 컴퓨팅사고 과제/04_27_9week2.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+x1 = [1,3,4,5,6,7,9]
+y1 = [4,7,2,4,7,8,3]
+
+x2 = [2,4,6,8,10]
+y2 = [5,6,2,6,2]
+
+plt.bar(x1, y1, label="Blue Bar", color='b')
+plt.bar(x2, y2, label="Green Bar", color='g')
+plt.plot()
+
+plt.xlabel("bar number")
+plt.ylabel("bar height")
+plt.title('Bar Chart Example')
+plt.legend()
+plt.show()
