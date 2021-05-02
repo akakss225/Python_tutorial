03fa95ad9071c6d2014dfed17b157dadb97191a5
akakss225 <akakss225@naver.com>
2021-05-02 21:18:49 +0900

    Data Structure_04_29_class3_MinHeap_미완

--- SCSC자구수업/04_29_class3.py
+++ SCSC자구수업/04_29_class3.py
@@ -26,16 +26,26 @@ class MinHeap:
         self.h[a], self.h[b] = self.h[b], self.h[a]
         
     def heapufy_down(self, idx):
-        while self.h[idx] < self.h[self.getParent(idx)]:
-            self.swap(idx, self.getParent(idx))
-            idx = self.getParent(idx)
+        while self.hasLchild(idx) or self.hasRchild(idx):
+            if self.h[idx] < self.h[self.getLchild(idx)] or self.h[idx] < self.h[self.getRchild(idx)]:
+                if self.h[self.getLchild(idx)] > self.h[self.getRchild(idx)]:
+                    self.swap(idx, self.getLchild(idx))
+                    idx = self.getLchild(idx)
+                else:
+                    self.swap(idx, self.getRchild(idx))
+                    idx = self.getRchild(idx)
             
     def insert(self, key):
         self.h.append(key)
-        self.heapufy_down(self.h.index(key))
+        self.heapufy_down(0)
         
     def getMin(self):
         return self.h[0]
+    
+    def delete_Min(self):
+        self.h.pop(0)
+        self.heapufy_down(0)
+        
     
         
         
@@ -50,5 +60,10 @@ h.insert(11)
 h.insert(10)
 h.insert(3)
 h.insert(1)
+h.insert(9)
 h.insert(8)
 print(h.h)
+
+h.delete_Min()
+print(h.h)
+
