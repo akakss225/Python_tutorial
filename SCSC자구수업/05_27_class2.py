d2641d36932867144e4eb173bce6fe845afbe957
akakss225 <akakss225@naver.com>
2021-05-27 20:52:30 +0900

    Data Structure_05_27_class2

---
+++ SCSC자구수업/05_27_class2.py
@@ -0,0 +1,46 @@
+class Dijkstra:
+    def __init__(self,nodes):
+        self.g = {}
+        self.dist = {}
+        for node in nodes:
+            self.g[node] = {}
+            self.dist[node] = [float("inf"), "none"]
+
+    def setEdge(self,a,b,w,bidirection=True):
+        self.g[a][b] = w
+        if bidirection == True: self.g[b][a] = w
+
+    def getPath(self,start,end):
+        dictfilt = lambda x, y: dict([(i, x[i]) for i in x if i in set(y)])
+        visits = set()
+        curNode = start
+        self.dist[curNode][0] = 0
+        while True:
+            visits.add(curNode)
+            nodes.remove(curNode)
+            neighbors = self.g[curNode]
+
+            for node in neighbors:
+                if min(self.dist[node][0], self.dist[curNode][0] + self.g[curNode][node]) < self.dist[node][0]:
+                    self.dist[node][0] = min(self.dist[node][0], self.dist[curNode][0] + self.g[curNode][node])
+                    self.dist[node][1] = curNode
+
+            if len(nodes) > 0:
+                curNode = min(dictfilt(self.dist, nodes), key=dictfilt(self.dist, nodes).get)
+            else:
+                break
+
+        path = [end]
+        dist = []
+
+        while end != start:
+            path.append(self.dist[end][1])
+            dist.append(self.dist[end][0])
+            end = self.dist[end][1]
+
+        return path[::-1], dist[::-1]
+
+import csv
+import folium
+import tkinter as tk
+import webbrowser
