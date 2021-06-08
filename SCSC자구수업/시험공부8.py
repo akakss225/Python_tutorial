6c54aae9680964d9a7d42cbb61ca56e88d8eb4ad
akakss225 <akakss225@naver.com>
2021-06-08 15:41:09 +0900

    Data Structure 시험공부8

---
+++ SCSC자구수업/시험공부8.py
@@ -0,0 +1,44 @@
+# Stack
+# Last in Last out
+
+class Stack:
+    def __init__(self):
+        self.s = []
+    
+    def size(self):
+        return len(self.s)
+    
+    def isEmpty(self):
+        if self.size() == 0:
+            return True
+        else:
+            return False
+    
+    def push(self, item):
+        self.s.append(item)
+    
+    def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.s.pop(-1)
+    
+    def peek(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.s[-1]
+
+s = Stack()
+
+
+print(s.isEmpty())
+print(s.peek())
+
+s.push('사과')
+
+print(s.isEmpty())
+print(s.peek())
+
+print(s.pop())
+print(s.peek())
