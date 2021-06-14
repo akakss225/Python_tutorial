28ca898db43f24692cd7104380b4ec6dc9132b1f
akakss225 <akakss225@naver.com>
2021-06-14 17:34:14 +0900

    Programmers_DFS/BFS_1_추가

--- 프로그래머스/DFS/BFS/num1.py
+++ 프로그래머스/DFS/BFS/num1.py
@@ -32,7 +32,20 @@ def solution(numbers, target):
     return answer
 
 
+
+def solution2(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution2(numbers[1:], target-numbers[0]) + solution2(numbers[1:], target+numbers[0])
+
+
 numbers = [1,1,1,1,1]
 target = 3
 
-print(solution(numbers, target))
+print(solution2(numbers, target))
+
+
+
