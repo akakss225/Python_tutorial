c25553dbd7b3eaeaf87a82e6753c119b4067387a
akakss225 <akakss225@naver.com>
2021-06-17 16:44:43 +0900

    Programmers_DFS/BFS_3_미완

--- 프로그래머스 이론/DFS.py
+++ 프로그래머스 이론/DFS.py
@@ -1,10 +1,11 @@
 # 깊이우선탐색, Stack을 이용해 구현함.
 
-def dfs(graph, v, visited):
+def dfs(graph, v, visited): # 정보가 기록된 graph와, 시작할 노드, 방문처리를 진행할 리스트 세가지를 인자로 사용한다.
     # 현재 노드를 방문 처리
     visited[v] = True
     print(v, end=" ")
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    # 현재 노드와 연결된 다른 노드를 전부 방문했다면, 이전에 스택에 쌓여있던 함수를 진행함.
     for i in graph[v]:
         if not visited[i]:
             dfs(graph, i , visited)
@@ -20,5 +21,6 @@ graph = [
     [2,6,8],
     [1,7]
 ]
+
 visited = [False] * 9
 dfs(graph, 1, visited)
