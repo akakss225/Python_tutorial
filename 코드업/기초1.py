b64b3157d1f4d7a3a7d1597090a2a4998c3e4b68
akakss225 <akakss225@naver.com>
2021-06-23 17:09:25 +0900

    기초 연습

---
+++ 코드업/기초1.py
@@ -0,0 +1,104 @@
+# c = float(input())
+# for i in range(3):
+#     print(c)
+
+# ---------------------
+
+
+# a,b = input().split()
+# print(a)
+# print(b)
+
+# ---------------------
+
+# a,b = input().split()
+
+# print(b, a)
+
+# ---------------------
+
+# a = input()
+
+# for i in range(3):
+#     print(a,end=" ")
+
+# ---------------------
+
+# a, b = input().split(":")
+
+# print(a,b,sep=":")
+
+# ---------------------
+
+# year, month, day = input().split(".")
+
+# print(day,month,year,sep="-")
+
+
+# ---------------------
+
+# a, b = input().split("-")
+
+# print(a+b)
+
+# ---------------------
+
+# a = input()
+
+# for i in range(len(a)):
+#     print(a[i])
+
+# ---------------------
+
+# a = input()
+
+# print(a[0:2], a[2:4],a[4:6])
+
+# ---------------------
+
+# hour, min, sec = input().split(":")
+
+# print(min)
+
+# ---------------------
+
+# a, b = input().split()
+
+# print(a+b)
+
+# ---------------------
+
+# a, b = input().split()
+
+# print(int(a) + int(b))
+
+# ---------------------
+
+# a = float(input())
+# b = float(input())
+
+# print(a + b)
+
+# ---------------------
+
+# a = int(input())
+
+# print('%x'%a)
+
+# ---------------------
+
+a = int(input())
+
+print('%X'%a)
+
+
+
+
+
+
+
+
+
+
+
+
