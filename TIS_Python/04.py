e85626440b392435d5c34632a328ef22bf5337e2
akakss225 <akakss225@naver.com>
2021-12-10 14:25:57 +0900

    TIS_Python class3_2

--- TIS_Python/04.py
+++ TIS_Python/04.py
@@ -1,7 +1,7 @@
-# 12 / 10 (fri) class 3 . 크롤링(파싱)
+# 12 / 10 (fri) class 4 . 크롤링(파싱)
 
 import csv
-f = open("seoul.csv", "r")
+f = open("seoul2.csv", "r")
 
 data = csv.reader(f)
 
@@ -13,12 +13,12 @@ max_temp = -999
 max_date = ""
 
 for row in data:
-    if row[4] == "":
-        row[4] = -999
-    row[4] = float(row[4])
-    if max_temp < row[4]:
-        max_date = row[2]
-        max_temp = row[4]
+    if row[-1] == "":
+        row[-1] = -999
+    row[-1] = float(row[-1])
+    if max_temp < row[-1]:
+        max_date = row[0]
+        max_temp = row[-1]
 f.close()
 
 print("서울의 기온이 가장 높았던 때는 ", max_date, "이며, ", max_temp, "도 였습니다.")
