6c58ea25a7a7dd31c6b2e7ba3f6bc05c8baa224f
akakss225 <akakss225@naver.com>
2021-12-10 14:24:21 +0900

    TIS_Python class3

--- TIS_Python/03.py
+++ TIS_Python/03.py
@@ -1,3 +1,24 @@
-# 12 / 10 (fri) class 3
+# 12 / 10 (fri) class 3 . 크롤링(파싱)
 
-asd
+import csv
+f = open("seoul.csv", "r")
+
+data = csv.reader(f)
+
+
+next(data) # 첫번째 행을 건너뜀
+
+
+max_temp = -999
+max_date = ""
+
+for row in data:
+    if row[4] == "":
+        row[4] = -999
+    row[4] = float(row[4])
+    if max_temp < row[4]:
+        max_date = row[2]
+        max_temp = row[4]
+f.close()
+
+print("서울의 기온이 가장 높았던 때는 ", max_date, "이며, ", max_temp, "도 였습니다.")
