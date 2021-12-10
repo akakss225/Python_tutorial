a6ad2ac70e3fdc7c664d9cbdc34f3ee0911d70a7
akakss225 <akakss225@naver.com>
2021-12-10 10:25:08 +0900

    TIS_Python class1

---
+++ TIS_Python/01.py
@@ -0,0 +1,24 @@
+## 기본 문법
+
+print(7 // 4)
+print(3 ** 4)
+
+
+multiline = """
+    Life is too short
+    You need Python
+"""
+
+print(multiline)
+
+print("=" * 50)
+
+print("학사관리")
+
+print("=" * 50)
+
+a = "Life is too short, You need Python"
+
+print(a[1])
+print(a[12])
+print(a[-1])
--- test.py
+++ test.py
@@ -1,21 +1,18 @@
-from collections import deque
-
-def solution(n):
-    answer = ""
-    temp = n
-    while temp != 0:
-        share = temp // 3
-        trash = temp % 3
-        if trash != 0:
-            if trash == 1:
-                answer = "1" + answer
+def solution(s):
+    while s:
+        check = len(s)
+        for i in range(0,len(s)):
+            if i != 0:
+                pre = s[i-1]
             else:
-                answer = "2" + answer
-        else:
-            answer = "4" + answer
-            share -= 1
-        temp = share
-    return answer
-
-print(solution(5))
-    
+                pre = ''
+            current = s[i]
+            if pre == current:
+                s.replace(pre+current,"",1)
+                break
+        check_after = len(s)
+        if check == check_after:
+            return 0
+    return 1
+s = "abbabb"
+print(solution(s))
