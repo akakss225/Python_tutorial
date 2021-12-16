b051d25e00ee6d5c863133fba5c66c55a3ff44a9
akakss225 <akakss225@naver.com>
2021-12-16 15:29:26 +0900

    TIS_Python 12 / 16_ 딥러닝_ 이론

---
+++ TIS_Python/1216_05_DEEP2.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# 인공 신경망 ANN Artificial Neural Network
+# 심층 신경망 DNN Deep Neural Network
+# 합성곱 신경망 CNN Convolution Neural Network : 이미지를 훈련할 때 주로 사용
+
+# 순방향 신경망 FNN FeedForward Neural Network >> 인공 신경망 / 심층 신경망 / 합성곱 신경망 등이 포함 될 수 있다.
+# 순환 신경망 RNN Recurrent Neural Network : 텍스트를 훈련할 때 주로 사용
+
+# 케라스 딥러닝 쾽순서
+# 1. 데이터 준비
+# 2. 모델 만들기
+# 3. 모델 컴파일
+# 4. 훈련/학습
+# 5. 평가(evaluate)
+
+
+
+
+
+
+
+
+
