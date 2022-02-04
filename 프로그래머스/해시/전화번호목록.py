cda2dd608362a58c62e6b6aff7a13991afac4985
akakss225 <akakss225@naver.com>
2022-02-04 18:46:11 +0900

    programmers Hash 전화번호 시간초과 2

--- 프로그래머스/해시/전화번호목록.py
+++ 프로그래머스/해시/전화번호목록.py
@@ -1,58 +1,24 @@
-# 문제 설명
-# 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
-# 전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
-
-# 구조대 : 119
-# 박준영 : 97 674 223
-# 지영석 : 11 9552 4421
-# 전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
-
-# 제한 사항
-# phone_book의 길이는 1 이상 1,000,000 이하입니다.
-# 각 전화번호의 길이는 1 이상 20 이하입니다.
-# 같은 전화번호가 중복해서 들어있지 않습니다.
-# 입출력 예제
-# phone_book	return
-# ["119", "97674223", "1195524421"]	false
-# ["123","456","789"]	true
-# ["12","123","1235","567","88"]	false
-# 입출력 예 설명
-# 입출력 예 #1
-# 앞에서 설명한 예와 같습니다.
-
-# 입출력 예 #2
-# 한 번호가 다른 번호의 접두사인 경우가 없으므로, 답은 true입니다.
-
-# 입출력 예 #3
-# 첫 번째 전화번호, “12”가 두 번째 전화번호 “123”의 접두사입니다. 따라서 답은 false입니다.
-
-
-'''
-시간 초과 2 개
-def solution(phone_book):
-    numbers = sorted(phone_book, key=lambda x : len(x),reverse=True)
-    
-    while numbers:
-        cur = numbers.pop()
-        for i in numbers:
-            if cur in i[0:len(cur)]:
-                return False
-    return True
-    
-'''
 from collections import deque
-
 def solution(phone_book):
-    
-    phone_book.sort(key = len)
-    numbers = deque(phone_book)
-    while numbers:
-        cur = numbers.popleft()
-        for i in numbers:
-            if cur in i[0:len(cur)]:
+    q = deque(sorted(phone_book, key=len))
+    while q:
+        cur = q.popleft()
+        for i in q:
+            if i[0:len(cur)] == cur:
                 return False
     return True
+
+def solution(phone_book):
+    d = dict()
+    phone_book.sort(key = len)
     
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][0:len(phone_book[i])]:
+            return False
+        
+    return True
+
+
 
 
 phone = ["119", "97674223",  '9763' ,"195524421"]
