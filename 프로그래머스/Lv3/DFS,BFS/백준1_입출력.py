66f02d1ea6632d3b88ea86755f5481510cadcf37
akakss225 <akakss225@naver.com>
2022-03-09 13:41:10 +0900

    백준 1260 BFS/DFS구현 입출력

---
+++ 프로그래머스/Lv3/DFS,BFS/백준1_입출력.py
@@ -0,0 +1,55 @@
+import sys
+from collections import defaultdict
+from collections import deque
+
+read = sys.stdin.readline
+
+n, m, v = map(int, read().split())
+g = []
+for i in range(m):
+    x, y = map(int, read().split())
+    g.append([x, y])
+
+def dfs(v, g):
+    g.sort(reverse=True)
+    d = defaultdict(list)
+    for x, y in g:
+        d[x].append(y)
+        d[y].append(x)
+    
+    s = [v]
+    visit = []
+    while s:
+        cur = s.pop()
+        
+        if cur not in visit:
+            visit.append(cur)
+            print(cur, end=" ")
+            if cur in d:
+                s.extend(d[cur])
+
+def bfs(v, g):
+    g.sort()
+    d = defaultdict(list)
+    for x, y in g:
+        d[x].append(y)
+        d[y].append(x)
+    
+    q = deque([v])
+    visit = []
+    
+    while q:
+        cur = q.popleft()
+        
+        if cur not in visit:
+            visit.append(cur)
+            print(cur, end=" ")
+            if cur in d:
+                q.extend(d[cur])
+
+dfs(v, g)
+print()
+bfs(v, g)
+
+
+
