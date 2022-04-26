bb71ae8dece7b7422819a0724b636f1c59b2737d
akakss225 <akakss225@naver.com>
2022-04-26 14:56:29 +0900

    programmers Lv2 빛의경로사이클 실패

--- 프로그래머스/Lv2/빛의 경로 사이클.py
+++ 프로그래머스/Lv2/빛의 경로 사이클.py
@@ -1,3 +1,6 @@
+from aem import con
+
+
 def path(grid, d):
     answer = []
     x = len(grid[0])
@@ -138,8 +141,156 @@ def solution(grid):
     return sorted(result)
 
 
+def path(grid, d):
+    answer = []
+    x = len(grid[0])
+    y = len(grid)
+    g = []
+    
+    for i in range(y):
+        temp = []
+        for j in range(x):
+            temp.append([[i, j], grid[i][j]])
+        g.append(temp)
+        
+    
+    answer.append(d)
+    while True:
+        direction = answer[-1]
+        nextY = direction[0][0]
+        nextX = direction[0][1]
+        dir = direction[1]
+        
+        if g[direction[0][0]][direction[0][1]][1] == "S":
+            if direction[1] == "UP":
+                if direction[0][0] + 1 == y:
+                    nextY = 0
+                    dir = "UP"
+                else:
+                    nextY = direction[0][0] + 1
+                    dir = "UP"
+            elif direction[1] == "DOWN":
+                if direction[0][0] - 1 == -1:
+                    nextY = y - 1
+                    dir = "DOWN"
+                else:
+                    nextY = direction[0][0] - 1
+                    dir = "DOWN"
+            elif direction[1] == "LEFT":
+                if direction[0][1] + 1 == x:
+                    nextX = 0
+                    dir = "LEFT"
+                else:
+                    nextX = direction[0][1] + 1
+                    dir = "LEFT"
+            elif direction[1] == "RIGHT":
+                if direction[0][1] - 1 == -1:
+                    nextX = x - 1
+                    dir = "RIGHT"
+                else:
+                    nextX = direction[0][1] - 1
+                    dir = "RIGHT"
+        elif g[direction[0][0]][direction[0][1]][1] == "L":
+            if direction[1] == "UP":
+                if direction[0][1] + 1 == x:
+                    nextX = 0
+                    dir = "RIGHT"
+                else:
+                    nextX = direction[0][1] + 1
+                    dir = "RIGHT"
+            elif direction[1] == "DOWN":
+                if direction[0][1] - 1 == -1:
+                    nextX = x - 1
+                    dir = "LEFT"
+                else:
+                    nextX = direction[0][1] - 1
+                    dir = "LEFT"
+            elif direction[1] == "LEFT":
+                if direction[0][0] - 1 == -1:
+                    nextY = y - 1
+                    dir = "DOWN"
+                else:
+                    nextY = direction[0][0] - 1
+                    dir = "DOWN"
+            elif direction[1] == "RIGHT":
+                if direction[0][0] + 1 == y:
+                    nextY = 0
+                    dir = "UP"
+                else:
+                    nextY = direction[0][0] + 1
+                    dir = "UP"
+        else:
+            if direction[1] == "UP":
+                if direction[0][1] - 1 == -1:
+                    nextX = x - 1
+                    dir = "RIGHT"
+                else:
+                    nextX = direction[0][1] - 1
+                    dir = "RIGHT"
+            elif direction[1] == "DOWN":
+                if direction[0][1] + 1 == x:
+                    nextX = 0
+                    dir = "LEFT"
+                else:
+                    nextX = direction[0][1] + 1
+                    dir = "LEFT"
+            elif direction[1] == "LEFT":
+                if direction[0][0] + 1 == y:
+                    nextY = 0
+                    dir = "UP"
+                else:
+                    nextY = direction[0][0] + 1
+                    dir = "UP"
+            elif direction[1] == "RIGHT":
+                if direction[0][0] - 1 == -1:
+                    nextY = y - 1
+                    dir = "DOWN"
+                else:
+                    nextY = direction[0][0] - 1
+                    dir = "DOWN"
+        nextDirection = [[nextY,nextX], dir]
+        
+        if nextDirection in answer:
+            break
+        else:
+            answer.append(nextDirection)
+    
+    return answer
+
+def solution(grid):
+    # 상 하 좌 우
+    u = path(grid, [[0, 0], 'UP'])
+    d = path(grid, [[0, 0], 'DOWN'])
+    l = path(grid, [[0, 0], 'LEFT'])
+    r = path(grid, [[0, 0], 'RIGHT'])
+    com = [d, l, r]
+    answer = [u]
+    result = [len(u)]
+    
+    
+    if len(grid) > 1:
+        i = 0
+        while com:
+            if i < len(com):
+                if answer[-1][0] in com[i]:
+                    com.pop(i)
+                i += 1
+            else:
+                i = 0
+                if com:
+                    answer.append(com.pop(0))
+                else:
+                    break
+        
+    else:
+        result = [len(u), len(d), len(l), len(r)]
+    
+    return sorted(result)
+
+
+
 
 grid = ["SL","LR"]
 grid = ["SL","LL"]
-# grid = ["R","R"]
+grid = ["R","R"]
 print(solution(grid))
