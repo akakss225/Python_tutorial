d8d8213f5fe0ef59589e4ba160b9e2b7f2b06284
akakss225 <akakss225@naver.com>
2023-02-04 16:31:42 +0900

    하샤드 수

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
+arr = ['1', '2']
 
-tmp = 0
-cnt = 0
-lt, rt = 0, 0
-while lt <= n - 1:
-  tmp += numList[rt]
-  if tmp < m:
-    rt += 1
-    if rt >= n :
-      break
-  else:
-    if tmp == m:
-      cnt += 1
-    tmp = 0
-    lt += 1
-    rt = lt
-print(cnt)
+print(sum(arr))
